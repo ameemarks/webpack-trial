20ce98c803ce4ad74566fd5bc72e9183426cf90d
Anna Grochola <agrochola@cyfrowypolsat.pl>
2017-06-08 22:24:53 +0200

    First commit

---
+++ entry.js
@@ -0,0 +1,3 @@
+require("./css/style.css");
+require("./css/style-ipla.css");
+document.write(require('./content.js'));
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/,
+                // include: [
+                //     path.resolve(__dirname, "entry.js")
+                //     // path.resolve(__dirname, "css/style-ipla.css")
+                // ],
+                loader: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                    publicPath: "",
+                use: [
+                    {
+                        loader: "css-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }
+                    // , {
+                    //     loader: 'postcss-loader'}
+                ]
+            }) }
+        ]
+      /*  rules: [
+            {
+                test: /\.css\/\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                        loader: "css-loader",
+                        options: {
+                            minimize: true
+                        }
+                        }, {
+                        loader: 'postcss-loader'}
+                        ]
+                })
+            }
+        ]*/
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+        /*new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css\/\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { discardComments: {removeAll: true } },
+            canPrint: true
+        })*/
+    ]
+};
