065c18b107b8bb76bc97a5e220c23acb589457a5
Anna Grochola <agrochola@cyfrowypolsat.pl>
2017-06-09 15:26:20 +0200

    Replace all

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 query: {
                     search: 'green',
                     replace: 'blue',
-                    flags: ''
+                    flags: 'g'
                 }
             }
         ]
